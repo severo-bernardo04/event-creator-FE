d074f25ee2deed248a7bb8af681cbe0a9e5c1954
lucas-delazeri <lucasdelazeris@gmail.com>
2026-04-13 11:40:41 -0300

    feat(ui): add Footer component with centered layout

--- my-app/app/components/Footer.tsx
+++ my-app/app/components/Footer.tsx
@@ -1,7 +1,11 @@
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="w-full border-t">
+      <div className="max-w-5xl mx-auto px-4 text-center">
+        Footer
+      </div>
+    </footer>
+  );
+};
 
 export default Footer
