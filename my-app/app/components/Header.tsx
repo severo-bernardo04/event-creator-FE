7a447fe5f5a8422b76ad83278d1387e36c657f8c
lucas-delazeri <lucasdelazeris@gmail.com>
2026-04-13 11:41:23 -0300

    feat(ui): add Header component with centered layout

--- my-app/app/components/Header.tsx
+++ my-app/app/components/Header.tsx
@@ -1,7 +1,11 @@
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="w-full border-b">
+      <div className="max-w-5xl mx-auto px-4 text-center">
+        Header
+      </div>
+    </header>
+  );
+};
 
 export default Header
