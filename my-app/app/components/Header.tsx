211214d3d0cb45c65638936e3dc92f18dc5c12e5
lucas-delazeri <lucasdelazeris@gmail.com>
2026-04-14 11:52:09 -0300

    fix(header): fix navigation and alignment issues

--- my-app/app/components/Header.tsx
+++ my-app/app/components/Header.tsx
@@ -1,7 +1,9 @@
 "use client";
+
 import { useState } from "react";
 import styles from "./Header.module.css";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 const Header = () => {
   const [active, setActive] = useState("eventos");
@@ -9,20 +11,36 @@ const Header = () => {
 
   return (
     <header className="w-full border-b">
-      <div>
-        <ul className="flex gap-4 justify-center py-4">
-          <li className={`${styles.HeaderElement} 
-                          ${active === "eventos" ? styles.active : ""}`
-                          } onClick={() => {setActive("eventos");router.push("/dashboard/events")}}>
+      <div className="flex items-center justify-between max-w-5xl mx-auto px-4 py-4">
+
+        <Image src="/home.png" alt="Home" width={24} height={24} className="cursor-pointer" onClick={() => router.push("/dashboard")}/>
+
+        <ul className="flex gap-4 justify-center">
+          <li
+            className={`${styles.HeaderElement} ${
+              active === "eventos" ? styles.active : ""
+            }`}
+            onClick={() => {
+              setActive("eventos");
+              router.push("/dashboard/events");
+            }}
+          >
             Lista de eventos
           </li>
 
-          <li className={`${styles.HeaderElement}
-                          ${active === "inscricoes" ? styles.active : ""}`
-                          } onClick={() => {setActive("inscricoes");router.push("/dashboard/inscriptions")}}>
+          <li
+            className={`${styles.HeaderElement} ${
+              active === "inscricoes" ? styles.active : ""
+            }`}
+            onClick={() => {
+              setActive("inscricoes");
+              router.push("/dashboard/inscriptions");
+            }}
+          >
             Minhas inscrições
           </li>
         </ul>
+        <div className="w-6" />
       </div>
     </header>
   );
