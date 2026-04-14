72ca389478602eeca5c285c9fbc5df703e145550
lucas-delazeri <lucasdelazeris@gmail.com>
2026-04-14 11:13:07 -0300

    feat: header with navigation between inscriptions and events list

--- my-app/app/components/Header.tsx
+++ my-app/app/components/Header.tsx
@@ -1,11 +1,31 @@
+"use client";
+import { useState } from "react";
+import styles from "./Header.module.css";
+import { useRouter } from "next/navigation";
+
 const Header = () => {
+  const [active, setActive] = useState("eventos");
+  const router = useRouter();
+
   return (
     <header className="w-full border-b">
-      <div className="max-w-5xl mx-auto px-4 text-center">
-        Header
+      <div>
+        <ul className="flex gap-4 justify-center py-4">
+          <li className={`${styles.HeaderElement} 
+                          ${active === "eventos" ? styles.active : ""}`
+                          } onClick={() => {setActive("eventos");router.push("/dashboard/events")}}>
+            Lista de eventos
+          </li>
+
+          <li className={`${styles.HeaderElement}
+                          ${active === "inscricoes" ? styles.active : ""}`
+                          } onClick={() => {setActive("inscricoes");router.push("/dashboard/inscriptions")}}>
+            Minhas inscrições
+          </li>
+        </ul>
       </div>
     </header>
   );
 };
 
-export default Header
+export default Header;
